94aa2a820ec581893a7dc26e8029749ee0bb9043
rdwrcode <rdwrcode@gmail.com>
2017-06-02 12:39:51 -0600

    racket-1

--- code/section5/83_racket_intro.rkt
+++ code/section5/83_racket_intro.rkt
@@ -10,3 +10,67 @@
 
 ; basic definitions
 (define s "hello")
+
+(define x 3)
+(define y (+ x 2)) ; + is a function, call it here
+(define cube1
+  (lambda (x)
+    (* x (* x x))))
+
+(define cube2
+  (lambda (x)
+    (* x x x)))
+
+(define (cube3 x)
+  (* x x x))
+
+(define (pow1 x y)
+  (if (= y 0)
+      1
+      (* x (pow1 x (- y 1)))))
+
+; curry
+(define pow2
+  (lambda (x)
+    (lambda (y)
+      (pow1 x y))))
+
+(define three-to-the (pow2 3))
+
+(define (sum xs)
+  (if (null? xs)
+      0
+      (+ (car xs) (sum (cdr xs)))))
+
+(define (my-append xs ys)
+  (if (null? xs)
+      ys
+      (cons (car xs) (my-append (cdr xs) ys))))
+
+(define (my-map f xs)
+  (if (null? xs)
+      null
+      (cons (f (car xs))
+            (my-map f (cdr xs)))))
+
+(define foo (my-map (lambda (x) (+ x 1))
+                    (cons 3 (cons 4 (cons 5 null)))))
+
+(define (square x) (* x x))
+(define (bar x xs) (cons x xs))
+
+(define (sum1 xs)
+  (if (null? xs)
+      0
+      (if (number? (car xs))
+          (+ (car xs) (sum1 (cdr xs)))
+          (+ (sum1 (car xs)) (sum1 (cdr xs))))))
+
+(define (sum2 xs)
+  (if (null? xs)
+      0
+      (if (number? (car xs))
+          (+ (car xs) (sum2 (cdr xs)))
+          (if (list? (car xs))
+              (+ (sum2 (car xs)) (sum2 (cdr xs)))
+              (sum2 (cdr xs))))))
--- racket/section5.md
+++ racket/section5.md
@@ -1,6 +1,32 @@
 # Section5: basics
 
+## 
 - [ ] based on scheme, but some non-compitable changes
 - [ ] CTRL-e to cycle through different windows with DrRacket
+- [ ] Empty list: null, cons, car (head) cdr (tail), null? (list e1...en)
 
+## 
+- [ ] syntax and parentheses: a term
+- [ ] an atom, #t, #f, 34, "hi", null, 4.0, x, ...
+- [ ] a special form: define, lambda, if (macro to define your own)
+- [ ] a sequence of terms in parens: (t1 t2 ... tn)
+- [ ] if t1 a special form, semantics of sequence is special
+- [ ] else a function call
+
+## 
+- [ ] DrRacket [] ()
+- [ ] text to a parsed tree: atoms are leaves, leaves are atoms which are not special forms
+- [ ] (e) call e with zero arguments
+
+##
+- [ ] 
+```
+      (cond [e1a e1b]
+            [e2a e2b]
+            ...
+            [eNa eNb]) eNa should be #t
+```
+if not #f, then all true
+
+## local binding
 
